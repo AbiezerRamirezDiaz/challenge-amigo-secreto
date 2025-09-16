18abd956cb85cc67b0ac61aaa954a149617e8185
Abiezer Ramirez Diaz <elreyaby@gmail.com>
2025-09-16 03:55:17 -0400

    Actualiza el archivo JS: Capturar el valor,Validar la entrada, actualizar el array, Limpiar el campo y Actualizar la visualización.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Capturar el valor del campo de entrada:
+let listaDeAmigos = [];
+//Validar la entrada:
+function agregarAmigo() {
+let entradaDeAmigo = document.getElementById("amigo");
+let nombreDeAmigo = entradaDeAmigo.value.trim();
+if (nombreDeAmigo === "") {
+    alert("Por favor, ingresa un nombre válido.");
+    return;
+}
+//Actualizar el array de amigos:
+listaDeAmigos.push(nombreDeAmigo);
+entradaDeAmigo.value = "";
+
+//Limpiar el campo de entrada:
+entradaDeAmigo.value = "";
+//Actualizar la visualización:
+let listaHTML = document.getElementById("listaAmigos");
+let nuevoItem = document.createElement("li");
+nuevoItem.textContent = nombreDeAmigo;
+listaHTML.appendChild(nuevoItem);
+}
